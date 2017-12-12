0ed49b15071a92a98dab9ded6cc98b89df04127d
Paul Anderson <paul.anderson@uberops.com>
2017-12-12 06:59:07 -0500

    :sparkles: adding some test secrets and passing secrets to jq

--- putSecrets.sh
+++ putSecrets.sh
@@ -6,10 +6,11 @@ while [ "$#" -gt 0 ]; do
   case "$1" in
     -p|--path) path="$2"; shift 2;;
     -*) echo "Unknown option: $1" >&2; exit 1;;
-    *) secrets_filename="$1"; shift 1; echo "secrets_filename is "$secrets_filename;;
+    *) secrets_filename="$1"; shift 1;;
   esac
 done
 
-path=${path:-$(uuidgen)}
+path=$(echo /${path:-$(uuidgen)}/ | tr -s '/' )
+secrets_filename=${secrets_filename:-'secrets.json'}
 
-echo "path is "$path
+jq -r '.' < $secrets_filename
