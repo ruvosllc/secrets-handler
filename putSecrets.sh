9ab027c8e043867e460a2be9236a547fa38bc5ed
Paul Anderson <paul.anderson@uberops.com>
2017-12-12 13:47:56 -0500

    :sparkles: adding logging and quiet option

--- putSecrets.sh
+++ putSecrets.sh
@@ -6,18 +6,27 @@ while [ "$#" -gt 0 ]; do
   case "$1" in
     -p|--path) path="$2"; shift 2;;
     -o|--overwrite) overwrite="--overwrite"; shift 1;;
+    -q|--quiet) quiet=true; shift 1;;
     -*) echo "Unknown option: $1" >&2; exit 1;;
     *) secrets_filename="$1"; shift 1;;
   esac
 done
 
-path=$(echo /${path:-$(uuidgen)}/ | tr -s '/' )
+if [[ -z $path ]]; then
+  path=$(uuidgen)
+  echo "No path provided. Using "$path
+fi
+path=$(echo /$path/ | tr -s '/' )
 secrets_filename=${secrets_filename:-'secrets.json'}
 overwrite=${overwrite:-''}
+quiet=${quiet:-''}
 
 paths_to_leaves=( $(jq -r 'paths(scalars) | map(strings, numbers|tostring) | join(".")' $secrets_filename) )
 leaves=( $(jq -r 'getpath(paths(scalars))' $secrets_filename) )
 
 for i in ${!paths_to_leaves[@]}; do
-  aws ssm put-parameter --name $path${paths_to_leaves[$i]} --type "SecureString" --value ${leaves[$i]} $overwrite
+  name=$path${paths_to_leaves[$i]}
+  value=${leaves[$i]}
+  aws ssm put-parameter --name $name --type "SecureString" --value $value $overwrite
+  if [[ -z $quiet ]]; then echo "Put value \"$value\" at parameter \"$name\""; fi
 done
