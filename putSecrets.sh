40125dd074828beedae79aaf58949b58fc99e2bc
Paul Anderson <paul.anderson@uberops.com>
2017-12-12 13:18:25 -0500

    :sparkles: put params!  :boom:

--- putSecrets.sh
+++ putSecrets.sh
@@ -5,6 +5,7 @@ set -e
 while [ "$#" -gt 0 ]; do
   case "$1" in
     -p|--path) path="$2"; shift 2;;
+    -o|--overwrite) overwrite="--overwrite"; shift 1;;
     -*) echo "Unknown option: $1" >&2; exit 1;;
     *) secrets_filename="$1"; shift 1;;
   esac
@@ -12,6 +13,11 @@ done
 
 path=$(echo /${path:-$(uuidgen)}/ | tr -s '/' )
 secrets_filename=${secrets_filename:-'secrets.json'}
+overwrite=${overwrite:-''}
 
 paths_to_leaves=( $(jq -r 'paths(scalars) | map(strings, numbers|tostring) | join(".")' $secrets_filename) )
 leaves=( $(jq -r 'getpath(paths(scalars))' $secrets_filename) )
+
+for i in ${!paths_to_leaves[@]}; do
+  aws ssm put-parameter --name $path${paths_to_leaves[$i]} --type "SecureString" --value ${leaves[$i]} $overwrite
+done
