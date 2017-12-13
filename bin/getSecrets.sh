05bcdc63a63df3b7c1d94feb679964c1d262b027
Paul Anderson <paul.anderson@uberops.com>
2017-12-13 06:06:52 -0500

    :bug: fix hardcoded test string derp

--- bin/getSecrets.sh
+++ bin/getSecrets.sh
@@ -11,4 +11,4 @@ done
 
 path=$(echo /$path/ | tr -s '/' )
 res=$(aws ssm get-parameters-by-path --path $path --with-decryption)
-echo $res | jq '.Parameters | map(.Name | ltrimstr("/test/paul/") | split(".") | map(if test("^[0-9]+$") then tonumber else . end)) as $paths | map(if .Value == "true" then true elif .Value == "false" then false else .Value end) as $values | [index(.[])] | reduce .[] as $index ({}; setpath($paths[$index];$values[$index]))'
+echo $res | jq '.Parameters | map(.Name | ltrimstr("'$path'") | split(".") | map(if test("^[0-9]+$") then tonumber else . end)) as $paths | map(if .Value == "true" then true elif .Value == "false" then false else .Value end) as $values | [index(.[])] | reduce .[] as $index ({}; setpath($paths[$index];$values[$index]))'
