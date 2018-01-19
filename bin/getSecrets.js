9f91252cb8211e34456d89008ea86b0c9dbf9708
Paul Anderson <paul.anderson@uberops.com>
2018-01-19 04:05:30 -0500

    :hammer: unify bins under one secrets-handler command and 3 subcommands

--- bin/getSecrets.js
+++ bin/getSecrets.js
@@ -1,20 +1,17 @@
 #!/usr/bin/env node
 
 const getSecrets = require('../lib/getSecrets')
-const commander = require('commander')
-const packageInfo = require('../package.json')
 
-commander
-.version(packageInfo.version)
-.option('-p, --path <path>', 'Specify the path prefix of the parameters to fetch.')
-.parse(process.argv)
+const action = (path) => {
+  getSecrets({ path })
+  .then(secrets => {
+    process.stdout.write(JSON.stringify(secrets, null, 2))
+    process.exit()
+  })
+  .catch(err => {
+    process.stderr.write(err.message)
+    process.exit(1)
+  })
+}
 
-getSecrets({ path: commander.path || '/' })
-.then(secrets => {
-  process.stdout.write(JSON.stringify(secrets, null, 2))
-  process.exit()
-})
-.catch(err => {
-  process.stderr.write(err.message)
-  process.exit(1)
-})
+module.exports = action
---
+++ bin/secrets-handler.js
@@ -0,0 +1,29 @@
+#!/usr/bin/env node
+
+const commander = require('commander')
+const packageInfo = require('../package.json')
+const putSecrets = require('./putSecrets')
+const getSecrets = require('./getSecrets')
+const deleteSecrets = require('./deleteSecrets')
+
+commander
+.version(packageInfo.version)
+
+commander
+.command('get <path>')
+.description('Fetch a collection of secrets from AWS SSM by path and write them to standard output as a single JSON object.')
+.action(getSecrets)
+
+commander
+.command('put <path>')
+.description('Read a secrets object from standard input and store it as individual parameters in AWS SSM.')
+.option('-o, --overwrite', 'Flag indicating that existing parameters should be overwritten.')
+.option('-k, --key-id <keyId>', 'Specify a KMS key to use when encrypting parameters. Defaults to your AWS account\'s default key.')
+.action(putSecrets)
+
+commander
+.command('delete <path>')
+.description('Delete a collection of secrets from AWS SSM by path and write the response to standard output.')
+.action(deleteSecrets)
+
+commander.parse(process.argv)
